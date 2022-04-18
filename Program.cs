35bdd83e8c75ee1f63e4cb0287cc8aa1d66fcb51
Stephen Cunningham <stevecunningham@outlook.com>
2022-04-17 23:31:25 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+           
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+
+
+            for(int i=0; i<5; i++)
+            {
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+                var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+                Console.WriteLine($"Kanye: {kanyeQuote}");
+
+                var ronResponse = client.GetStringAsync(ronURL).Result;
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+                Console.WriteLine($"Ron: {ronQuote}");
+                Console.WriteLine("----------------------------");
+            }
+
+            
+        }
+    }
+}
